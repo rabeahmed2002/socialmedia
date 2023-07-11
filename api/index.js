29bc1085e3a72d551c081b6f02ac0786bb6a7a2a
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-11 16:37:25 +0500

    api working

--- api/index.js
+++ api/index.js
@@ -1,2 +1,11 @@
-import { Express } from "express";
+import Express from "express";
+import userRoutes from "./routes/users.js"
 
+
+const app=Express()
+
+app.use("/api/users", userRoutes)
+
+app.listen(8800, ()=>{
+    console.log("API working");
+})
