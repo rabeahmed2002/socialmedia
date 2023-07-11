094bb7713cbc300920061cee2cf60e2b13604379
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-11 18:48:54 +0500

    nodejs mysql auth

--- api/index.js
+++ api/index.js
@@ -5,17 +5,23 @@ import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comment.js"
 import likeRoutes from "./routes/likes.js"
+
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
 const app=express()
 
 //middleware
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
-app.use("/api/auth", authRoutes)
 
 app.listen(8800, ()=>{
     console.log("API working");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,7 @@
-import express from "express"
-import {login, logout, register} from "../controllers/auth.js"
+import express from "express";
+import { login,register,logout } from "../controllers/auth.js";
 
-const router=express.Router()
-
+const router = express.Router()
 
 router.post("/login", login)
 router.post("/register", register)
