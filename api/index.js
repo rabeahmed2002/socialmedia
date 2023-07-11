5f70b8e0b24cb4612142cd6cb12294d308586572
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-12 01:05:40 +0500

    share and posts complete

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import commentRoutes from "./routes/comment.js"
 import likeRoutes from "./routes/likes.js"
 
 import cors from "cors"
+import multer from "multer";
 import cookieParser from "cookie-parser";
 
 const app=express()
@@ -25,7 +26,21 @@ app.use(
 );
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
