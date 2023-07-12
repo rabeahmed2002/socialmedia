e7105f38b5e6e2168969c6c25aed2b611200f3b1
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-12 23:19:33 +0500

    final commit

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './home.scss'
 
-import Stories from "../../components/Stories/stories"
 // import Posts from "../../components/Posts/Posts.jsx"
 import Posts from "../../components/Posts/posts"
 
@@ -9,7 +8,6 @@ import Posts from "../../components/Posts/posts"
 const home = () => {
   return (
     <div className='home'>
-      <Stories/>
       <Posts/>
 
     </div>
